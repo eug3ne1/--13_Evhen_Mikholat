bca1d59f636fb89fb53eb8f589bdea83c825b16d
DESKTOP-FSJS10U\mihol <miholatevgen@gmail.com>
2023-10-23 14:21:42 +0300

    no message

---
+++ labs/src/lab2/Main.java
@@ -0,0 +1,13 @@
+// Заліковка  №1318
+//C5 = 1318 % 5; // C = A xor B
+//C7 = 1318 % 7; // type - short
+//C11 = 1318 % 11; //Знайти середнє значення елементів кожного стовпчика матриця
+
+package lab2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+    }
+}
